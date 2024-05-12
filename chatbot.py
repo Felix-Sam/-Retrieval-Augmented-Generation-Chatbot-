2b01368d1b671bfe33b8d59d5ec9fae7c8931b51
felix sam <felixsam922@gmail.com>
2024-05-12 21:03:05 +0100

    version one of telesambot

---
+++ chatbot.py
@@ -0,0 +1,80 @@
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_pinecone import PineconeVectorStore
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+from fastapi import FastAPI
+from pydantic import BaseModel
+from langchain_groq import ChatGroq
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# API_KEY = os.getenv('OPENAI_API_KEY')
+# MODEL = 'gpt-3.5-turbo'
+# model = ChatOpenAI(api_key=API_KEY,model=MODEL)
+
+parser = StrOutputParser()
+
+telesam_key = os.getenv('telesam_key')
+model_grok = ChatGroq(api_key =telesam_key,model_name = 'gemma-7b-it')
+print(telesam_key )
+
+loader = TextLoader('data.txt',encoding = 'utf-8')
+document = loader.load()
+document_spliter = RecursiveCharacterTextSplitter(chunk_size=2000,chunk_overlap = 500)
+data = document_spliter.split_documents(document)
+
+template = ("""
+You are AI-powered chatbot designed to provide information and assistance for customers
+based on the context provided to you only. In case you don't know what user's are 
+requesting for just let them know. Note do not start answers with The provided text contains nor end with it.
+Context:{context}
+Question:{question}
+""")
+
+prompt = PromptTemplate.from_template(template=template)
+prompt.format(
+    context = ' Here is a context to use',
+    question = ' This is a question to answer'
+)
+
+# # For sending the data to the database. it should be run only once.
+# pinecone_index_name = 'telesam'
+# embeddings = OpenAIEmbeddings()
+# pinecone = PineconeVectorStore.from_documents(data,embedding=embeddings,index_name = pinecone_index_name)
+
+# For retrieving data from the database.
+pinecone_index_name = 'telesam'
+embeddings = OpenAIEmbeddings()
+pinecone = PineconeVectorStore(embedding=embeddings,index_name = pinecone_index_name)
+
+retriever = pinecone.as_retriever()
+result = RunnableParallel(context =retriever,question = RunnablePassthrough())
+chain = result |prompt | model_grok | parser
+
+
+app  = FastAPI()
+class Data(BaseModel):
+    text: str 
+
+@app.post('/prompt')
+async def prompts(data:Data):
+    response = chain.invoke(data.text)
+    return {'response': response}
+
+@app.post('/add_data')
+async def add_data(data:Data):
+    try:
+        pinecone.add_texts([data.text])
+        return 'sucessfully added'
+    except:
+        return 'failed to add data'
+    
+
+
---
+++ testapp.py
@@ -0,0 +1,50 @@
+import requests
+import streamlit as st
+
+def text_to_speech(text):
+    response = requests.post(
+    'https://api.v6.unrealspeech.com/stream',
+    headers = {
+        'Authorization' : 'Bearer UTDGSkrdGvKrm6GPx7CwaD5haA8UibBNONKN1kyKCfYqC8udXZP7FM'
+    },
+    json = {
+        'Text': text, # Up to 1000 characters
+        'VoiceId': 'Scarlett', # Dan, Will, Scarlett, Liv, Amy
+        'Bitrate': '320k', # 320k, 256k, 192k, ...
+        'Speed': '0.2', # -1.0 to 1.0
+        'Pitch': '1', # -0.5 to 1.5
+        'Codec': 'libmp3lame', # libmp3lame or pcm_mulaw
+    }
+    )
+    return response.content
+    
+
+# Define the URL of your chatbot API
+API_URL = "https://chatbot-27je.onrender.com/prompt"
+# Function to send a message to the chatbot API and get a response
+def send_message(message):
+    try:
+        response = requests.post(API_URL, json={"text": message})
+        json_response = response.json()
+        if "response" in json_response:
+            return json_response["response"]
+        else:
+            return None
+    except Exception as e:
+        st.write('Connection error try again')
+
+# Streamlit app code
+def main():
+    st.title("Bhuman.AI Chatbot Interface")
+    # Create a text input for user to enter messages
+    user_input = st.text_input("Enter a message")
+
+    # Send the user's message to the chatbot API when a button is clicked
+    if st.button("Send"):
+        bot_response = send_message(user_input)
+        if bot_response is not None:
+            st.text_area("Bot's Reply", value=bot_response, height=200)
+            audio = text_to_speech(bot_response)
+            st.audio(audio)
+if __name__ == "__main__":
+    main()
